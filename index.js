8abb4526af880f0ad13d528ed19d54ae15e51bd1
Mariana Usuga <marianausuga275004@outlook.es>
2021-06-22 18:54:20 -0500

    better gallery events with JavaScript

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 
+
+const imgLight=document.querySelector(".img-1")
+
 $(".img-gallery").click(function(){
+    var src=$(this).attr("src")
+    imgLight.src=src;
     $(".imagen-light").addClass("showImg")
 
-});
+})
 $(".close").click(function(){
     $(".imagen-light").removeClass("showImg")
 });
+
+
+
+
+
+
+/*function showImg(id){
+    switch(id){
+        case 'img1':
+
+    }
+    $("img").attr("src","img/"+id+".png")
+
+    
+}*/
